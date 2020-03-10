9986156968be40f2e45620c827a27bd22875246f
elliefairholm <elliefairholm@gmail.com>
2020-03-10 21:35:50 +0100

    feat: link to github added to homepage bla

--- src/components/presentational/homepage-info-tile.vue
+++ src/components/presentational/homepage-info-tile.vue
@@ -5,8 +5,10 @@
     </div>
 
     <div class="basic-info">
-      Barcelona based Full-Stack Developer specialising in Javascript. Experienced in Node, both relational and non-relational databases GraphQL, Anuglar, React and Typescript. Always interested in learning new technologies. In fact, I learnt Vue in less than a week, and used it for the front-end of this website.
+      Barcelona based Full-Stack Developer specialising in Javascript. Experienced in Node, both relational and non-relational databases GraphQL, Anuglar, React and Typescript. Always interested in learning new technologies. In fact, I learnt Vue in less than a week, and used it for the front-end of this website. You can take a look at my github repository here:
       <br />
+      <br />
+      <a class="link">https://github.com/elliefairholm/personal-website</a>
       <br />Currently mastering: Vue.js.
       <br />
       <br />I'm highly motived and always the optimist. I like facing the unknown and coming up with new solutions to challenging problems.
@@ -37,7 +39,13 @@ export default {
 .basic-info {
   width: 60%;
   display: flex;
+  flex-direction: column;
   text-align: center;
   align-items: center;
+}
+
+.link {
+  text-decoration: none;
+  color: maroon;
 }
 </style>

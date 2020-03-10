a0ff384339a22ad380949d5b72f7b957ad932ae9
elliefairholm <elliefairholm@gmail.com>
2020-03-10 19:37:22 +0100

    feat: cta button added

---
+++ src/components/presentational/cta-button.vue
@@ -0,0 +1,28 @@
+<template>
+  <button class="cta-button">{{ text }}</button>
+</template>
+
+<script>
+export default {
+  name: "cta-button",
+  props: {
+    text: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.cta-button {
+  border: 2px maroon solid;
+  background: maroon;
+  color: white;
+  width: 110px;
+  padding: 5px;
+  font-weight: 500;
+  margin-left: 5px;
+}
+</style>

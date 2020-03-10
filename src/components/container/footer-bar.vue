866bd5a4909e27ae6c258867abb86b09e4b231ef
elliefairholm <elliefairholm@gmail.com>
2020-03-10 19:21:26 +0100

    feat: added header and footer

---
+++ src/components/container/footer-bar.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="footer">
+    <div class="logo-links">
+      <a href="mailto:elliefairholm@gmail.com">
+        <img class="icon" src="../../assets/email-icon.png" alt="Email Icon" />
+      </a>
+      <a href="https://www.linkedin.com/in/elliefairholm">
+        <img class="icon" src="../../assets/linkedin-logo.jpg" alt="LinkedIn Logo" />
+      </a>
+      <a href="https://github.com/elliefairholm">
+        <img class="icon" src="../../assets/github-logo.svg" alt="Github Logo" />
+      </a>
+      <a href="https://www.instagram.com/elliefairholm">
+        <img class="icon" src="../../assets/instagram-logo.png" alt="Instagram Logo" />
+      </a>
+    </div>
+    <div class="copyright">Copyright © 2020.</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "footer-bar",
+  data() {
+    return {
+      profilePicture: "../../assets/me.jpeg"
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.footer {
+  display: flex;
+  justify-content: space-between;
+  width: 80%;
+}
+
+.icon {
+  height: 25px;
+}
+</style>
---
+++ src/components/container/navbar.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="navbar">
+    <div class="header-logo-container">
+      <img style="width:auto" src="../../assets/name.png" alt="Ellie Fairholm Logo" />
+    </div>
+    <div class="sections">
+      <router-link
+        :to="{page}"
+        v-for="(page, index) in pages"
+        :key="index"
+        class="uppercase"
+        :class="{selected: page === selectedPage}"
+        @click="() => updatePage(page)"
+      >{{ page === '/' ? 'home' : page.slice(1) }}</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "navbar",
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      pages: ["/", "/blog", "/cv", "/contact"]
+    };
+  },
+  methods: {
+    updatePage(page) {
+      this.selectedPage = page;
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.navbar {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  align-items: center;
+}
+
+.sections {
+  display: flex;
+  justify-content: space-evenly;
+  width: 100%;
+  padding-top: 20px;
+}
+
+.uppercase {
+  text-transform: uppercase;
+}
+
+.header-logo-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 80%;
+  border-bottom: 1px lightgray solid;
+  padding-bottom: 15px;
+}
+
+.selected {
+  border-bottom: 3px maroon solid;
+}
+</style>

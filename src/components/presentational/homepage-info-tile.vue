6e5677edffa3f61ed3007544273bb5373c3439af
elliefairholm <elliefairholm@gmail.com>
2020-03-10 19:31:25 +0100

    feat: homepage info tile added

---
+++ src/components/presentational/homepage-info-tile.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="homepage">
+    <div class="profile-picture-container">
+      <img class="profile-picture" src="../../assets/me.jpeg" alt="Ellie Fairholm" />
+    </div>
+
+    <div class="basic-info">
+      Barcelona based Full-Stack Developer specialising in Javascript. Experienced in Node, both relational and non-relational databases GraphQL, Anuglar, React and Typescript. Always interested in learning new technologies. In fact, I learnt Vue in less than a week, and used it for the front-end of this website.
+      <br />
+      <br />Currently mastering: Vue.js.
+      <br />
+      <br />I'm highly motived and always the optimist. I like facing the unknown and coming up with new solutions to challenging problems.
+      <br />
+      <br />Massive fan of the theatre, and can't stop tap dancing.
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "homepage"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.profile-picture {
+  height: 300px;
+}
+
+.homepage {
+  display: flex;
+  padding: 5% 10%;
+  justify-content: space-evenly;
+}
+
+.basic-info {
+  width: 60%;
+  display: flex;
+  text-align: center;
+  align-items: center;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <homepage-info-tile />
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import homepageInfoTile from "@/components/presentational/homepage-info-tile.vue";
 
 export default {
-  name: 'Home',
+  name: "homepageInfoTile",
   components: {
-    HelloWorld
+    "homepage-info-tile": homepageInfoTile
   }
-}
+};
 </script>

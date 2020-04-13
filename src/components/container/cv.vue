af33cf856b19f91e11196e891448207fc727b122
elliefairholm <elliefairholm@gmail.com>
2020-04-13 18:27:37 +0200

    feat: cv page updated

---
+++ src/components/container/cv.vue
@@ -0,0 +1,412 @@
+<template>
+  <div id="cv">
+    <h1 class="name font-3rem uppercase">ellie fairholm</h1>
+    <div class="details">
+      <a href="mailto:elliefairholm@gmail.com">
+        <p>elliefairholm@gmail.com</p>
+      </a>
+      <p>|</p>
+      <a href="http://elliefairholm.com" target="_blank">
+        <p>elliefairholm.com</p>
+      </a>
+      <p>|</p>
+      <p>+447964900622</p>
+      <p>|</p>
+      <a href="http://linkedin.com/in/elliefairholm" target="_blank">
+        <p>linkedin.com/in/elliefairholm</p>
+      </a>
+      <p>|</p>
+      <a href="http://github.com/elliefairholm" target="_blank">
+        <p>github.com/elliefairholm</p>
+      </a>
+    </div>
+    <div class="description">
+      <p>Full-Stack Developer specialising in Javascript. Always curious to learn new technologies, and everything learnt forms part of my blog series called 'What I learnt this week'. Highly motivated and always positive. Flexible approach and effective communicator in order to meet demanding deadlines.</p>
+    </div>
+    <div class="section-container">
+      <div class="section-title">skills</div>
+      <div class="skills">
+        <div class="skill-line">
+          <div>
+            <p class="bubble">MongoDB</p>
+            <p />
+          </div>
+          <div>
+            <p class="bubble">React</p>
+            <p />
+          </div>
+          <div>
+            <p class="bubble">Vue</p>
+            <p />
+          </div>
+          <div>
+            <p class="bubble">Optimism</p>
+            <p />
+          </div>
+        </div>
+        <div class="skill-line">
+          <div>
+            <p class="bubble">Node.js</p>
+            <p />
+          </div>
+          <div>
+            <p class="bubble">SQL</p>
+            <p />
+          </div>
+          <div>
+            <p class="bubble">Angular</p>
+            <p />
+          </div>
+          <div>
+            <p class="bubble">Teamwork</p>
+            <p />
+          </div>
+        </div>
+        <div class="skill-line">
+          <div>
+            <p class="bubble">Redis</p>
+            <p />
+          </div>
+          <div>
+            <p class="bubble">GraphQL</p>
+            <p />
+          </div>
+          <div>
+            <p class="bubble">Koa.js</p>
+            <p />
+          </div>
+          <div>
+            <p class="bubble">Adaptability</p>
+            <p />
+          </div>
+        </div>
+        <div class="skill-line">
+          <div>
+            <p class="bubble">Express.js</p>
+            <p />
+          </div>
+          <div>
+            <p class="bubble">Jest</p>
+            <p />
+          </div>
+          <div>
+            <p class="bubble">Cypress.io</p>
+            <p />
+          </div>
+          <div>
+            <p class="bubble">Communication</p>
+            <p />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="section-container">
+      <div class="section-title">professional experience</div>
+      <div class="section-content">
+        <div class="content-example">
+          <div class="example-information">
+            <p>
+              <span class="bold">ABI Health Spain</span>,
+              <span class="italic">Client Integration Developer</span>
+            </p>
+            <ul class="example-details">
+              <li>A virtual health assistant that connects you to a global network of doctors for quick answers or in-depth consultations</li>
+              <li>Responsibilities: Integrating the ABI application into incoming clients' systems.</li>
+              <li>Tech Stack: React, Node, GraphQL.</li>
+            </ul>
+          </div>
+          <div class="example-date-location">
+            <p class="date">2020 - present</p>
+            <p class="location">Barcelona</p>
+          </div>
+        </div>
+        <div class="content-example">
+          <div class="example-information">
+            <p>
+              <span class="bold">The Book of Everyone</span>,
+              <span class="italic">Digital Marketing and Partnerships Co-ordinator</span>
+            </p>
+            <ul class="example-details">
+              <li>A personalised book gifting company.</li>
+              <li>
+                Responsibilities: B2C and B2B marketing, acquiring partnership avenues,
+                organised business trips. Learnt adaptability and presentational abilities.
+              </li>
+              <li>Platforms: Google Ads, Not On The Highstreet, and Etsy.</li>
+            </ul>
+          </div>
+          <div class="example-date-location">
+            <p class="date">2017 - 2018</p>
+            <p class="location">Barcelona</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="section-container">
+      <div class="section-title">education</div>
+      <div class="section-content">
+        <div class="content-example">
+          <div class="example-information">
+            <p>
+              <span class="bold">Codeworks</span>,
+              <span class="italic">Europe's Leading Javascript Bootcamp</span>
+            </p>
+            <ul class="example-details">
+              <li>Intensive 3 month, 65 hrs a week programming course.</li>
+              <li>
+                Wide Range of Front-End and Back-End Frameworks, as well as Authorisation and
+                Authentication (JWT, OAuth), and an introduction to DevOps (Docker).
+              </li>
+            </ul>
+          </div>
+          <div class="example-date-location">
+            <p class="date">2019 - 2020</p>
+            <p class="location">Barcelona</p>
+          </div>
+        </div>
+        <div class="content-example">
+          <div class="example-information">
+            <p>
+              <span class="bold">University of Exeter</span>,
+              <span class="italic">History and Spanish</span>
+            </p>
+            <ul class="example-details">
+              <li>Mastering of essay writing techniques and foreign language skills.</li>
+              <li>Extensive study of 20th Century social world history.</li>
+              <li>Spanish fluency with a year abroad in Barcelona.</li>
+            </ul>
+          </div>
+          <div class="example-date-location">
+            <p class="date">2015 - 2019</p>
+            <p class="location">Exeter</p>
+          </div>
+        </div>
+        <div class="content-example">
+          <div class="example-information">
+            <p>
+              <span class="bold">St George's College Weybridge</span>,
+              <span class="italic">A* A* A</span>
+            </p>
+            <ul class="example-details">
+              <li>Academic Scholarship.</li>
+              <li>Numerous Foreign Exchange Programs.</li>
+              <li>Mentor to younger and new students.</li>
+            </ul>
+          </div>
+          <div class="example-date-location">
+            <p class="date">2008 - 2015</p>
+            <p class="location">London</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="section-container">
+      <div class="section-title">projects</div>
+      <div class="section-content">
+        <div class="content-example">
+          <div class="example-information">
+            <p>
+              <span class="bold">noREST</span>,
+              <span class="italic">Full-Stack Software Engineer</span>
+            </p>
+            <ul class="example-details">
+              <li>A service platform that lets you create a comprehensive no-code API in minutes.</li>
+              <li>
+                Responsibilities: led the back-end team and designed the underlying data
+                infrastructure. Organised the data flow, handled caching, and implemented a drag
+                and drop service. Learnt the importance of communication and optimism.
+              </li>
+              <li>Tech stack: React, Redux, Material UI, Node.js, Koa, Mongoose, MongoDB, Redis.</li>
+            </ul>
+          </div>
+          <div class="example-date-location">
+            <p class="date">2020 - 2020</p>
+            <p class="location">Barcelona</p>
+          </div>
+        </div>
+        <div class="content-example">
+          <div class="example-information">
+            <p>
+              <span class="bold">Magic Mirror</span>,
+              <span class="italic">Front-End Software Engineer</span>
+            </p>
+            <ul class="example-details">
+              <li>
+                An open-source widget module for MagicMirror² that displays a daily historical fact
+                using my own public API.
+              </li>
+              <li>Tech Stack: Node.js, Koa, Mongoose, MongoDB, Raspbian.</li>
+            </ul>
+          </div>
+          <div class="example-date-location">
+            <p class="date">2020 - 2020</p>
+            <p class="location">Barcelona</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="section-container">
+      <div class="section-title">languages</div>
+      <div class="table-three">
+        <div class="no-margin">
+          <p class="bold">English</p>
+          <p class="italic">Native Language.</p>
+        </div>
+        <div class="no-margin">
+          <p class="bold">Spanish</p>
+          <p class="italic">Full working proficiency - C1.</p>
+        </div>
+        <div class="no-margin">
+          <p class="bold">French</p>
+          <p class="italic">Limited working proficiency - B1.</p>
+        </div>
+      </div>
+    </div>
+    <div class="section-container">
+      <div class="section-title">interests</div>
+      <div class="table-three">
+        <div class="no-margin">
+          <p class="bold">Tap Dancing</p>
+          <p class="italic">Advanced 2 ISTD qualification.</p>
+        </div>
+        <div class="no-margin">
+          <p class="bold">Ballet</p>
+          <p class="italic">Grade 8 RAD qualification.</p>
+        </div>
+        <div class="no-margin">
+          <p class="bold">Hockey</p>
+          <p class="italic">County level, and national school finals.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "cv"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#cv {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+}
+
+.details {
+  display: flex;
+  justify-content: space-evenly;
+  flex-wrap: wrap;
+  width: 80%;
+}
+
+.name {
+  color: #05386b;
+  text-transform: capitalize;
+}
+
+.section-title {
+  font-size: 2rem;
+  color: #05386b;
+  margin-bottom: 2%;
+  text-transform: capitalize;
+}
+
+.section-container {
+  width: 100%;
+  margin-bottom: 2.5%;
+}
+
+.content-example {
+  display: grid;
+  grid-template-columns: 6fr 1fr;
+  grid-template-rows: 1fr;
+}
+
+.description {
+  text-align: center;
+}
+
+.example-information {
+  padding-right: 10%;
+}
+
+.example-details > * {
+  margin: 0;
+}
+
+.example-date-location > * {
+  margin: 0;
+}
+
+.date {
+  margin-top: 16px;
+}
+
+.font-3rem {
+  font-size: 3rem;
+}
+
+.bold {
+  font-weight: 500;
+}
+
+.italic {
+  font-style: italic;
+}
+
+ul {
+  list-style: "- ";
+}
+
+.skills {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  margin-top: 2.5%;
+}
+
+.skill-line {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+}
+
+.skills > * {
+  margin: 0 auto;
+}
+
+.bubble {
+  background: #ffc64b;
+  color: white;
+  border-radius: 5px;
+  width: 150px;
+  text-align: center;
+  margin: 1%;
+}
+
+.evenly-spaced {
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+}
+
+.table-three {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-rows: 1fr;
+}
+
+.no-margin > * {
+  margin-top: 0;
+  margin-bottom: 0;
+}
+
+a {
+  text-decoration: none;
+}
+</style>
--- src/components/presentational/cta-button.vue
+++ src/components/presentational/cta-button.vue
@@ -1,5 +1,8 @@
 <template>
-  <button class="cta-button">{{ text }}</button>
+  <div class="cta-button">
+    <button class="button">{{ text }}</button>
+    <img class="robot" :src="require('../../assets/robot-arms-extended.png')" alt="walle robot" />
+  </div>
 </template>
 
 <script>
@@ -17,9 +20,13 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .cta-button {
+  position: relative;
+}
+
+.button {
   all: unset;
-  border: 2px #c70808 solid;
-  background: #c70808;
+  position: absolute;
+  background: #ffc64b;
   color: white;
   width: 110px;
   padding: 5px;
@@ -27,5 +34,12 @@ export default {
   margin-left: 5px;
   text-align: center;
   border-radius: 5px;
+  margin-top: 38px;
+}
+
+.robot {
+  height: 150px;
+  width: 150px;
+  margin-left: 80px;
 }
 </style>
